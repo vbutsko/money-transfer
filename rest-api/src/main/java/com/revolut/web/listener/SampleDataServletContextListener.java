08496b917bb0f5edb20c78faec4c17d37ed97957
Vlad <vbutsko@workfusion.com>
2020-02-26 01:38:45 +0300

    init data changes

--- rest-api/src/main/java/com/revolut/web/listener/SampleDataServletContextListener.java
+++ rest-api/src/main/java/com/revolut/web/listener/SampleDataServletContextListener.java
@@ -1,12 +1,13 @@
 package com.revolut.web.listener;
 
-import javax.servlet.ServletContextEvent;
-import javax.servlet.ServletContextListener;
 import java.math.BigDecimal;
 import java.time.LocalDateTime;
+import javax.servlet.ServletContextEvent;
+import javax.servlet.ServletContextListener;
 
 import com.revolut.dao.account.AccountDao;
 import com.revolut.dao.account.InMemoryAccountDao;
+import com.revolut.dao.exception.DaoValidationException;
 import com.revolut.dao.model.Account;
 import com.revolut.dao.model.Currency;
 import com.revolut.dao.model.Transaction;
@@ -15,22 +16,44 @@ import com.revolut.dao.transation.InMemoryTransactionDao;
 import com.revolut.dao.transation.TransactionDao;
 
 public class SampleDataServletContextListener implements ServletContextListener {
+
     private final static String INSERT_SAMPLE_DATA_PARAMETER = "insertSampleData";
+
     @Override
     public void contextInitialized(ServletContextEvent servletContextEvent) {
+        try {
+            initData(servletContextEvent);
+        } catch (DaoValidationException ex) {
+            throw new RuntimeException("Problems during init db");
+        }
+    }
+
+    @Override
+    public void contextDestroyed(ServletContextEvent servletContextEvent) {
+
+    }
+
+    private void initData(ServletContextEvent servletContextEvent) throws DaoValidationException {
         String insertSampleData = servletContextEvent.getServletContext().getInitParameter(INSERT_SAMPLE_DATA_PARAMETER);
 
         if (Boolean.valueOf(insertSampleData)) {
-            AccountDao accountDao  = InMemoryAccountDao.getInstance();
-            TransactionDao transactionDao  = InMemoryTransactionDao.getInstance();
+            AccountDao accountDao = InMemoryAccountDao.getInstance();
+            TransactionDao transactionDao = InMemoryTransactionDao.getInstance();
             Account account1 = accountDao.save(Account.builder()
                     .uuid("account-1")
                     .total(BigDecimal.valueOf(1000L))
                     .currency(Currency.USD)
                     .build());
+            Account account2 = accountDao.save(Account.builder()
+                    .uuid("account-2")
+                    .total(BigDecimal.valueOf(2000L))
+                    .currency(Currency.USD)
+                    .build());
+
             transactionDao.save(Transaction.builder()
                     .uuid("transfer-1")
-                    .accountId(account1.getId())
+                    .ownerAccountId(account1.getId())
+                    .otherAccountId(account2.getId())
                     .currency(Currency.USD)
                     .createdAt(LocalDateTime.now())
                     .amount(BigDecimal.valueOf(100L))
@@ -39,7 +62,8 @@ public class SampleDataServletContextListener implements ServletContextListener 
                     .build());
             transactionDao.save(Transaction.builder()
                     .uuid("transfer-2")
-                    .accountId(account1.getId())
+                    .ownerAccountId(account1.getId())
+                    .otherAccountId(account2.getId())
                     .currency(Currency.USD)
                     .createdAt(LocalDateTime.now())
                     .amount(BigDecimal.valueOf(10L))
@@ -47,14 +71,10 @@ public class SampleDataServletContextListener implements ServletContextListener 
                     .description("Transfer")
                     .build());
 
-            Account account2 = accountDao.save(Account.builder()
-                    .uuid("account-2")
-                    .total(BigDecimal.valueOf(2000L))
-                    .currency(Currency.USD)
-                    .build());
             transactionDao.save(Transaction.builder()
                     .uuid("transfer-3")
-                    .accountId(account2.getId())
+                    .ownerAccountId(account2.getId())
+                    .otherAccountId(account1.getId())
                     .currency(Currency.USD)
                     .createdAt(LocalDateTime.now())
                     .amount(BigDecimal.valueOf(500L))
@@ -64,8 +84,4 @@ public class SampleDataServletContextListener implements ServletContextListener 
         }
     }
 
-    @Override
-    public void contextDestroyed(ServletContextEvent servletContextEvent) {
-
-    }
 }
